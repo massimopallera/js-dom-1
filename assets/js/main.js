61e9988a9a309d231f906342d466937f7c77d0ee
massimopallera <pallera.massimo@gmail.com>
2024-10-08 14:42:52 +0200

    add: img+button elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-console.log('it works');
+const lamp = document.getElementById('lamp')
+const btn = document.getElementById('cta')
+
+const open = "./assets/img/yellow_lamp.png"
+const close = "./assets/img/white_lamp.png"
+
+const openStr = 'Accendi'
+const closeStr = 'Spegni'
+
+lamp.src = close
+btn.innerHTML = openStr
+
+
